a474f34418e6e4383b8abc40554c1b8bb66cf840
John Zlotek <jzlotek@gmail.com>
2018-12-05 16:04:33 -0500

    PR fixes

--- components/routes/github-authentication.js
+++ components/routes/github-authentication.js
@@ -1,45 +1,51 @@
-var debug = require('debug')('botkit:incoming_webhooks');
 var axios = require('axios')
+let debug = require('debug')('botkit:main')
+const octokit = require('@octokit/rest')
 
-const state = "fndsjgbfhusdabic457438hfb89b0qbd80n9u2n4qf80dxrwb0t"
 
 module.exports = function(webserver, controller) {
 
     debug('Configured /github/auth url');
     webserver.get('/github/auth', (req, res) => {
-        console.log('entered on /github/auth')
-        let code = req.query.code
-        let access_token = req.query.access_token
-        let token_type = req.query.token_type
+        debug('entered on /github/auth')
+        let { code, access_token, token_type } = req.query
+
         if (code) {
-            console.log(code)
-            // add OAuth code to db
-            // axios.post('https://github.com/login/oauth/access_token', {
-            //     client_id: `${process.env.GITHUB_CLIENT_ID}`,
-            //     client_secret: `${process.env.GITHUB_CLIENT_ID}`,
-            //     code: `${code}`,
-            //     redirect_uri: 'http://localhost:3000/github/auth',
-            //     state: state
-            // })
-            axios.post(`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}&state=${state}&redirect_uri=http://localhost:3000/github/auth`)
+            debug('Bot Code: ', code);
+                        // add OAuth code to db
+            axios.post(`https://github.com/login/oauth/access_token` +
+                        `?client_id=${process.env.GITHUB_CLIENT_ID}` +
+                        `&client_secret=${process.env.GITHUB_CLIENT_SECRET}` +
+                        `&code=${code}` +
+                        `&state=${process.env.STATE}` +
+                        `&redirect_uri=http://localhost:3000/github/auth`
+            )
+
             .then(result => {
-                console.log(result.data.split('&').forEach(e => {
-                    console.log(e.split('='))
-                }))
-            })
+                debug(result.data)
+                result.data.split('&').forEach(e => {
+                    debug(e.split('='))
+                })
+
+            }).catch(err => debug(err))
+
 
         } else if (access_token && token_type) {
-            console.log(access_token, token_type)
+            debug(access_token, token_type)
         } else {
-            console.log("resonse with no code")
+            debug("response with no code")
         }
-
-        // res.redirect('/')
+        
+        res.redirect('/')
     })
     
     webserver.get('/github', (req, res) => {
-        console.log('entered on /github')
-        return res.redirect(`https://github.com/login/oauth/authorize?scope=user:email,read:user,read:org&client_id=${process.env.GITHUB_CLIENT_ID}&state=${state}`)
+        debug('entered on /github')
+        return res.redirect(`https://github.com/login/oauth/authorize` +
+            `?scope=user:email,read:user,read:org` +
+            `&client_id=${process.env.GITHUB_CLIENT_ID}` +
+            `&state=${process.env.STATE}`
+        )
     })
 
 }
