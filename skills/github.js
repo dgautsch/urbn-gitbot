e921ddf44c380a1e95e564b4fd850cbe9a5465c8
Michael Greenberg <mgreenberg@urbn.com>
2018-12-06 13:51:32 -0500

    Mock data (#16)
    * Add mock data
    
    * Add updated mock with reviewers

--- skills/github.js
+++ skills/github.js
@@ -1,39 +1,37 @@
 const debug = require('debug')('message:github');
-const axios = require('axios');
+const data = require('../fixtures/data');
+
 
 function gitHubAuth(controller) {
 
     controller.on('get_github_prs', (bot, message) => {
         // @todo add logic to make the GitHub request for PRs.
-        axios.get('https://api.github.com/repos/mgreenberg5/URBN-GITBOT/pulls')
-            .then((result) => {
-                let openPrs = '';
-                if (result) {
-                    result.data.forEach((pr) => {
-                        /* eslint-disable-next-line */
-                        const { title, url } = pr;
-                        /* eslint-disable-next-line */
-                        openPrs += `<${url}|${title}>, \n`;
-                    });
-                }
-
-                bot.reply(message, {
-                    attachments: [
-                        {
-                            title: 'GitHub PRs',
-                            pretext: 'Here are your PRs for review.',
-                            text: openPrs,
-                            mrkdwn_in: ['text', 'pretext'],
-                        },
-                    ],
-                }, (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Experiment finished');
-                    }
-                });
+        let openPrs = '';
+        if (data) {
+            data.forEach((pr) => {
+                /* eslint-disable-next-line */
+                const { title, url } = pr;
+                /* eslint-disable-next-line */
+                openPrs += `<${url}|${title}>, \n`;
             });
+        }
+
+        bot.reply(message, {
+            attachments: [
+                {
+                    title: 'GitHub PRs',
+                    pretext: 'Here are your PRs for review.',
+                    text: openPrs,
+                    mrkdwn_in: ['text', 'pretext'],
+                },
+            ],
+        }, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Experiment finished');
+            }
+        });
     });
 
     controller.on('do_github_auth', (bot, message) => {
