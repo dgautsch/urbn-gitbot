b5723a7f4b6529bbe740e8f07ce1e5ae5d7cc9ad
Sean Kennedy <skennedy@urbn.com>
2018-12-06 11:15:06 -0500

    Adding ephemeral slash command response, adding contextual responses and channel join message

--- skills/sample_formatting.js
+++ skills/sample_formatting.js
@@ -9,46 +9,19 @@ module.exports = (controller) => {
     controller.on('slash_command', (bot, message) => {
 
         // reply to slash command
-        bot.replyPublic(message, {
-            "text": "Would you like to play a game?",
-            "attachments": [
-                {
-                    "text": "Choose a game to play",
-                    "fallback": "You are unable to choose a game",
-                    "callback_id": "wopr_game",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "game",
-                            "text": "Chess",
-                            "type": "button",
-                            "value": "chess"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Falken's Maze",
-                            "type": "button",
-                            "value": "maze"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Thermonuclear War",
-                            "style": "danger",
-                            "type": "button",
-                            "value": "war",
-                            "confirm": {
-                                "title": "Are you sure?",
-                                "text": "Wouldn't you prefer a good game of chess?",
-                                "ok_text": "Yes",
-                                "dismiss_text": "No"
-                            }
-                        }
-                    ]
-                }
-            ]
-        });
+        bot.replyInteractive(message, {
+            text: 'Can you keep a secret?',
+            replace_original: false,
+            response_type: 'ephemeral'
+          }, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('Experiment finished');
+            }
+          });
 
+        //bot.replyPublic(message,'Everyone can see this.');
         //bot.replyPrivate(message,'Only the person who used the slash command can see this.');
 
     });
